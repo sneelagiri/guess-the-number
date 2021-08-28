34646362d2909c65f4adba48c8add20feaa30598
Shashank Neelagiri <shashankn96@gmail.com>
2021-08-28 16:49:44 +0200

    added client logic

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import HandleGuess from './components/HandleGuess';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Guess the Number</h1>
+      <h2>Instructions</h2>
+      <p>Guess a number between 1 and 100. You have three attempts. You win the game if you are able to guess the number within three guesses. You lose and you have to restart if you do not.</p>
+      <HandleGuess></HandleGuess>
     </div>
   );
 }
---
+++ client/src/components/HandleGuess.js
@@ -0,0 +1,95 @@
+import React, { Component } from "react";
+
+export default class HandleGuess extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      numGuesses: 3,
+      randomNumber: 0,
+      userGuess: 0,
+      win: null,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    if ((this.state.numGuesses = 3)) {
+      const randomNumber = Math.floor(Math.random() * 100) + 1;
+      this.setState({
+        randomNumber: randomNumber,
+      });
+    }
+  }
+
+  evaluateGuess = () => {
+    if (this.state.randomNumber == this.state.userGuess) {
+      this.setState({ win: true });
+    } else if (this.state.numGuesses === 1) {
+      this.setState({ win: false });
+    } else {
+      this.decreaseGuesses();
+    }
+  };
+
+  decreaseGuesses = () => {
+    this.setState({
+      numGuesses: this.state.numGuesses - 1,
+    });
+  };
+
+  handleChange(event) {
+    this.setState({
+      userGuess: event.target.value,
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  playAgain = () => {
+    const randomNumber = Math.floor(Math.random() * 100) + 1;
+    this.setState({
+      numGuesses: 3,
+      win: null,
+      randomNumber: randomNumber
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        {this.state.win === null ? (
+          <div>
+            <h2>
+              Number of Guesses Left: {this.state.numGuesses}
+            </h2>
+            <form onSubmit={this.handleSubmit}>
+              <label>Your guess: </label>
+              <input
+                type="number"
+                name="guess"
+                value={this.state.userGuess}
+                onChange={this.handleChange}
+              />
+              <input type="submit" name="Submit" onClick={this.evaluateGuess} />
+              <button>Reset</button>
+            </form>
+          </div>
+        ) : this.state.win === false ? (
+          <div>
+            <h1>You lose. Please click reset to try again.</h1>
+            <button onClick={this.playAgain}>Play again</button>
+          </div>
+        ) : (
+          <div>
+            <h1>Congratulations! You won!</h1>
+            <button onClick={this.playAgain}>Play again</button>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
